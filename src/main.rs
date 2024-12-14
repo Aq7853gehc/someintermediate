7fa49dbe932ff7ccd88e823e274db56f78b80ad5
raziaquib423 <raziaquib423@gmail.com>
2024-12-14 16:09:03 +0000

    rust project is about to start

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+#![allow(unreachable_code, unused_labels)]
+
+mod book;
+use book::{bar, baz, foo};
+
+fn normal_loop() {
+    let mut count = 0u32;
+
+    println!("Let's count until infinity!");
+
+    // Infinite loop
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            // Skip the rest of this iteration
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            break;
+        }
+    }
+}
+
+fn nested_loop() {
+    'outer: loop {
+        println!("Entered the outer loop");
+
+        'inner: loop {
+            println!("Entered the inner loop");
+
+            // This would break only the inner loop
+            //break;
+
+            // This breaks the outer loop
+            break 'outer;
+        }
+
+        println!("This point will never be reached");
+    }
+
+    println!("Exited the outer loop");
+}
+
+fn return_loop() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    assert_eq!(result, 20);
+    println!("The result is {}", result);
+}
+
+fn main() {
+    foo();
+    bar();
+    baz();
+}

c33392a80b26615ad396151252b03316d2f04822
raziaquib423 <raziaquib423@gmail.com>
2024-12-15 07:28:29 +0000

    work with lib

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // fn main() {
-//  panic!("Crash and terminate the porgram")   
+//  panic!("Crash and terminate the porgram")
 // }
 
 // fn main() {
@@ -8,13 +8,39 @@
 //     println!("{}",v[99]);
 // }
 
-use std::fs::File;
+// use std::fs::File;
+
+// fn main() {
+//     let opne_file = File::open("hello.txt");
+//     let _open_file_resutl = match opne_file {
+//         Ok(file) => file,
+//         Err(error) => panic!("Problem opening the file: {:?}", error),
+//     };
+// }
+
+
+use app::NewsArticle;
+use app::Summary;
+use app::Tweet;
+
 
 
 fn main() {
-    let opne_file = File::open("hello.txt");
-    let _open_file_resutl = match opne_file {
-        Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
+    let artical = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
     };
+
+    let tweet:Tweet = Tweet{
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("{}", tweet.summarize());
+    println!("{}", artical.summarize());
+    
 }
